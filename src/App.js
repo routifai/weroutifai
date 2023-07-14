b3d242a182b4c902126a6cb086ff76ca0bd625a3
simo oulaidi <simooulaidi@simos-MacBook-Pro.local>
2023-07-14 00:30:38 -0400

    changes to test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          weRoutifAI Launching right now 5!!
+          weRoutifAI Launching right now 6!!
         </p>
         <a
           className="App-link"
